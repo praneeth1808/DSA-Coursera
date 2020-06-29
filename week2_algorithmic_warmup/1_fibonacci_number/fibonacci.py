0a0d27d38e440895a914134f3c850d56174d7c84
praneeth1808 <praneeth3331997@gmail.com>
2020-06-29 07:57:53 +0530

    week 2 : Fibonacci numbers solved

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,11 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    FebSeries=[]
+    FebSeries.append(0)
+    FebSeries.append(1)
+    for i in range(2,n+1):
+        FebSeries.append(FebSeries[i-1]+FebSeries[i-2])
+    return FebSeries[n]
 
 n = int(input())
 print(calc_fib(n))

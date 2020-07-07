39961dddfff146e1d5e09b24391033b8b7b7c37f
praneeth1808 <praneeth3331997@gmail.com>
2020-07-07 08:08:25 +0530

    week-3 : completed first three

--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -4,7 +4,19 @@ import sys
 
 def compute_min_refills(distance, tank, stops):
     # write your code here
-    return -1
+    i=0
+    numberofStops=-1
+    updated_stops=[0]+stops+[distance]
+    n=len(stops)
+    while i<=n:
+        [start,next_stop]=[updated_stops[i],updated_stops[i+1]]
+        i+=1
+        if next_stop-start>tank:
+            return -1
+        numberofStops+=1
+        while i<=n and updated_stops[i]-start<tank:
+            i+=1
+    return numberofStops
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())

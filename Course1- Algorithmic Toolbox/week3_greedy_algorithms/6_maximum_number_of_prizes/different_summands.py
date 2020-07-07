b7fbfae3f16c8318f4f3b1f6a22832ed3018815f
praneeth1808 <praneeth3331997@gmail.com>
2020-07-07 11:55:01 +0530

    week 3 : 6 programs completed

--- Course1- Algorithmic Toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ Course1- Algorithmic Toolbox/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,9 +1,12 @@
 # Uses python3
 import sys
-
+import math
 def optimal_summands(n):
     summands = []
     #write your code here
+    max_range=int(math.sqrt(2*n + 0.25)-0.5)
+    sum = int(max_range*(max_range+1)*0.5)
+    summands=[*range(1,max_range),max_range+n-sum]
     return summands
 
 if __name__ == '__main__':

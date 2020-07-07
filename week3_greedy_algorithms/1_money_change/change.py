52cdbc08aa1c6e51fb60c7accc18d58f26d420ba
praneeth1808 <praneeth3331997@gmail.com>
2020-07-07 06:40:48 +0530

    week-3 : completed first two

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -3,8 +3,15 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    coins=[10,5,1]
+    count=0
+    for i in coins:
+        if m==0:
+            return count
+        count+=m//i
+        m=m%i
+    return count
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -3,8 +3,24 @@ import sys
 
 def get_optimal_value(capacity, weights, values):
     value = 0.
+    length=len(weights)
+    A=[0]*length
     # write your code here
-
+    list_values=[]
+    for i in range(length):
+        list_values.append({
+            "weight":weights[i],
+            "value":values[i],
+            "VPU":values[i]/weights[i]
+        })
+    SortedList = sorted(list_values, key=lambda x: x['VPU'], reverse=True)
+    for i in range(length):
+        if capacity==0:
+            return value
+        a=min(SortedList[i]['weight'],capacity)
+        value+=a*(SortedList[i]['VPU'])
+        A[i]+=a
+        capacity-=a
     return value
 
 
@@ -14,4 +30,4 @@ if __name__ == "__main__":
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+    print("{:.4f}".format(opt_value))

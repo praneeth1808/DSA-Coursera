511e7c3cc6b3f6a34fb6b1cfc60fe8336dc08cf5
praneeth <praneeth3331997@gmail.com>
2021-02-08 10:46:20 +0530

    c1-w5,w6 updated from online

--- Course1- Algorithmic Toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ Course1- Algorithmic Toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -2,9 +2,17 @@
 
 import sys
 
-def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+def lcs2(a,b):
+    import numpy as np
+    dp = np.zeros((len(a)+1, len(b)+1)) #keep a matrix of size len(a)+1 and len(b)+1 first row and first column has 0s only as there is no matching at all. we use it for next rows and columns
+    
+    for i,number1 in enumerate(a):   #enumerate for index and values of both arrays, note that I am starting from index 0 but adding it as dp[i+1]dp[j+1]
+        for j,number2 in enumerate(b):
+            if number1 == number2:   # if numbers match, common sequence length increases by 1
+                dp[i+1][j+1] = dp[i][j]+1
+            else:  #else just take the max of the square just above or just left
+                dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+    return int(dp[len(a)][len(b)])
 
 if __name__ == '__main__':
     input = sys.stdin.read()

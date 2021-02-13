1bc62d3b2e1fbf1730b7ff2e3070e05a828dc22f
praneeth <praneeth3331997@gmail.com>
2021-02-13 19:17:11 +0530

    week1 - 2 done

--- Course2- Data Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ Course2- Data Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -14,17 +14,26 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append((i,next))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if len(opening_brackets_stack)<=0:
+                return i+1
+            prev=opening_brackets_stack.pop()
+            if (next==")" and prev[1]=="(") or (next=="}" and prev[1]=="{") or (next=="]" and prev[1]=="["):
+                pass
+            else:
+                return i+1
             pass
-
+    return "Success" if len(opening_brackets_stack)==0 else opening_brackets_stack[-1][0]+1
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
--- Course2- Data Structures/week1_basic_data_structures/2_tree_height/tree_height.py
+++ Course2- Data Structures/week1_basic_data_structures/2_tree_height/tree_height.py
@@ -4,23 +4,44 @@ import sys
 import threading
 
 
-def compute_height(n, parents):
-    # Replace this code with a faster implementation
-    max_height = 0
-    for vertex in range(n):
-        height = 0
-        current = vertex
-        while current != -1:
-            height += 1
-            current = parents[current]
-        max_height = max(max_height, height)
-    return max_height
+class TreeHeight:
+    def read(self):
+        self.n = int(sys.stdin.readline())
+        self.parent = list(map(int, sys.stdin.readline().split()))
 
+    def compute_height(self):
+        # Replace this code with a faster implementation
+        maxHeight = 0
+        for vertex in range(self.n):
+            height = 0
+            i = vertex
+            while i != -1:
+                height += 1
+                i = self.parent[i]
+            maxHeight = max(maxHeight, height);
+        return maxHeight;
+    def compute_height_fast(self):
+        heights=[0]*self.n
+        for vertex in range(self.n):
+            i=self.parent[vertex]
+            height=1
+            while i!=-1:
+                if heights[i]!=0:
+                    height+=heights[i]
+                    break
+                height+=1
+                i=self.parent[i]
+            while vertex!=-1 and heights[vertex]==0:
+                heights[vertex]=height
+                height-=1
+                vertex=self.parent[vertex]
+        return max(heights)
 
 def main():
-    n = int(input())
-    parents = list(map(int, input().split()))
-    print(compute_height(n, parents))
+    tree = TreeHeight()
+    tree.read()
+    # print(tree.compute_height())
+    print(tree.compute_height_fast())
 
 
 # In Python, the default limit on recursion depth is rather low,

1e73bfd2879fa90f34308db4edbc2607943fb84c
praneeth <praneeth3331997@gmail.com>
2021-03-02 15:59:23 +0530

    week 3 completed

--- Course2- Data Structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ Course2- Data Structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -1,32 +1,59 @@
 # python3
 
-from collections import namedtuple
+class JobQueue:
+    def read_data(self):
+        self.num_threads, m = map(int, input().split())
+        self.array = [[0] * 2 for i in range(self.num_threads)]
+        self.jobs = list(map(int, input().split()))
+        assert m == len(self.jobs)
+        self.threads = [None] * len(self.jobs)
+        self.start = [0] * len(self.jobs)
 
-AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
+    def initiate(self):
+        thread = 0
+        for i in range(min(self.num_threads, len(self.jobs))):
+            self.array[i] = [self.jobs[i], i]
+            self.threads[i] = thread
+            thread += 1
+        for i in range(self.num_threads // 2, -1, -1):
+            self.shiftDowm(i)
+
+    def print_response(self):
+        for i in range(len(self.jobs)):
+            print(self.threads[i], self.start[i])
+
+    def shiftDowm(self, i):
+        min_index = i
+        l = 2 * i + 1 if (2 * i + 1 < self.num_threads) else -1
+        r = 2 * i + 2 if (2 * i + 2 < self.num_threads) else -1
+        if (l != -1) and ((self.array[l][0] < self.array[min_index][0]) or (
+                (self.array[l][0] == self.array[min_index][0]) and self.array[l][1] < self.array[min_index][1])):
+            min_index = l
+        if (r != -1) and ((self.array[r][0] < self.array[min_index][0]) or (
+                (self.array[r][0] == self.array[min_index][0]) and (self.array[r][1] < self.array[min_index][1]))):
+            min_index = r
+        if i != min_index:
+            self.array[i], self.array[min_index] = \
+                self.array[min_index], self.array[i]
+            self.shiftDowm(min_index)
+
+    def assign_jobs(self):
+        self.initiate()
+        time = 0
+        if len(self.jobs) > self.num_threads:
+            for i in range(self.num_threads, len(self.jobs)):
+                self.shiftDowm(0)
+                time = self.array[0][0]
+                self.threads[i] = self.array[0][1]
+                self.start[i] = time
+                self.array[0][0] += self.jobs[i]
+
+    def solve(self):
+        self.read_data()
+        self.assign_jobs()
+        self.print_response()
 
 
-def assign_jobs(n_workers, jobs):
-    # TODO: replace this code with a faster algorithm.
-    result = []
-    next_free_time = [0] * n_workers
-    for job in jobs:
-        next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
-        result.append(AssignedJob(next_worker, next_free_time[next_worker]))
-        next_free_time[next_worker] += job
-
-    return result
-
-
-def main():
-    n_workers, n_jobs = map(int, input().split())
-    jobs = list(map(int, input().split()))
-    assert len(jobs) == n_jobs
-
-    assigned_jobs = assign_jobs(n_workers, jobs)
-
-    for job in assigned_jobs:
-        print(job.worker, job.started_at)
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    job_queue = JobQueue()
+    job_queue.solve()

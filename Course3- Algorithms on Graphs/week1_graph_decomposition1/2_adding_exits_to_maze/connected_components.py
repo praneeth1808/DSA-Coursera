d9fa278679735377e74b6171ad12de2e24e1085d
praneeth <praneeth3331997@gmail.com>
2021-03-22 22:36:37 +0530

    3- week-1 -program-2 done

--- Course3- Algorithms on Graphs/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
+++ Course3- Algorithms on Graphs/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -2,10 +2,18 @@
 
 import sys
 
+def explore(v):
+    visited[v]=1
+    for i in adj[v]:
+        if(visited[i]!=1):
+            explore(i)
 
-def number_of_components(adj):
+def number_of_components(n):
     result = 0
-    #write your code here
+    for i in range(n):
+        if visited[i]==0:
+            explore(i)
+            result+=1
     return result
 
 if __name__ == '__main__':
@@ -14,8 +22,11 @@ if __name__ == '__main__':
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    global adj
     adj = [[] for _ in range(n)]
+    global visited
+    visited = [0 for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(number_of_components(adj))
+    print(number_of_components(n))

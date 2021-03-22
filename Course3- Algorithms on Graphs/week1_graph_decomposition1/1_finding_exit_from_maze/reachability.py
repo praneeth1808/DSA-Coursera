4764e417cf492c092a45fdef90fd7808453e7a67
praneeth <praneeth3331997@gmail.com>
2021-03-22 21:38:33 +0530

    3- week-1 -program-1 done

--- Course3- Algorithms on Graphs/week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
+++ Course3- Algorithms on Graphs/week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
@@ -2,16 +2,18 @@
 
 import sys
 
-def reach(adj, x, y):
+def reach(visited,adj, x, y):
     if len(adj[x])==0:
         return 0
     for i in adj[x]:
-        adj[x]=adj[x][1:]
-        if i == y:
+        if i == y :
             return 1
-        return reach(adj,i,y)
+    visited[x]=1
+    for i in adj[x]:
+        if(visited[i]==0):
+            if reach(visited, adj, i, y) == 1:
+                return 1
     return 0
-
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
@@ -21,7 +23,8 @@ if __name__ == '__main__':
     x, y = data[2 * m:]
     adj = [[] for _ in range(n)]
     x, y = x - 1, y - 1
+    visited=[0 for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+    print(reach(visited,adj, x, y))

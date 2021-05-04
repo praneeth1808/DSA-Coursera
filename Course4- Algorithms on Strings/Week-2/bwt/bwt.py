5afaa9e9a1a82e28a96bab956985486344f2e96a
praneeth <praneeth3331997@gmail.com>
2021-05-04 18:20:23 +0530

    courser4: week3,4 initiated

--- Course4- Algorithms on Strings/Week-2/bwt/bwt.py
+++ Course4- Algorithms on Strings/Week-2/bwt/bwt.py
@@ -2,7 +2,16 @@
 import sys
 
 def BWT(text):
-    return ""
+    length = len(text)
+    text=2*text
+    matrix=[]
+    for i in range(length):
+        matrix.append(text[i:i+length])
+    matrix.sort()
+    BWT=""
+    for i in matrix:
+        BWT+=i[-1]
+    return BWT
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
--- Course4- Algorithms on Strings/Week-2/bwtinverse/bwtinverse.py
+++ Course4- Algorithms on Strings/Week-2/bwtinverse/bwtinverse.py
@@ -1,11 +1,44 @@
 # python3
 import sys
+import math
+
+def InverseBWT_fast(bwt):
+    # write your code here
+    last=[]
+    counter={"$":0,"A":0,"C":0,"G":0,"T":0}
+    for i in range(len(bwt)):
+        symbol = bwt[i]
+        last.append(symbol+str(counter[symbol]))
+        counter[symbol] += 1
+    first = []
+    for key in ["$","A","C","G","T"]:
+        for i in range(counter[key]):
+            first.append(key+str(i))
+    pos = 0
+    text = ["A"]*len(last)
+    first_to_last_map = dict(zip(first, last))
+    nextSymbol="$0"
+    for i in range(len(last)):
+        text[i]=nextSymbol[0]
+        nextSymbol=first_to_last_map[nextSymbol]
+    return "".join(text[::-1])
 
 def InverseBWT(bwt):
     # write your code here
-    return ""
-
+    last=[]
+    num=10**math.ceil(math.log10(len(bwt)))
+    for i in range(len(bwt)):
+        last.append(bwt[i]+str(num+i))
+    first=sorted(last)
+    pos=0
+    text=""
+    while True:
+        currentNode = first[pos]
+        text += currentNode[0]
+        pos = last.index(currentNode)
+        if pos == 0:
+            return text[1:]
 
 if __name__ == '__main__':
     bwt = sys.stdin.readline().strip()
-    print(InverseBWT(bwt))
+    print(InverseBWT_fast(bwt))

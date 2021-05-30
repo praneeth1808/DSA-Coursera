640111227ef4c300de92bde149bb7b0ce1aa4cdf
praneeth <praneeth3331997@gmail.com>
2021-05-30 07:58:03 +0530

    course 5: week - 3 completed

--- Course5- Advanced Algorithms and Complexity/Week - 3/Programming-Assignment-3/cleaning_apartment/cleaning_apartment.py
+++ Course5- Advanced Algorithms and Complexity/Week - 3/Programming-Assignment-3/cleaning_apartment/cleaning_apartment.py
@@ -1,14 +1,45 @@
 # python3
+
+import time
+
 n, m = map(int, input().split())
-edges = [ list(map(int, input().split())) for i in range(m) ]
+edges = [list(map(int, input().split())) for i in range(m)]
+
 
 # This solution prints a simple satisfiable formula
 # and passes about half of the tests.
 # Change this function to solve the problem.
 def printEquisatisfiableSatFormula():
-    print("3 2")
-    print("1 2 0")
-    print("-1 -2 0")
-    print("1 -2 0")
+    clauses = []
+    for i in range(n):
+        v_pos = [(i * n) + j for j in range(1, n + 1)]
+        clauses.append(v_pos)
+        for j in range(0, n):
+            for k in range(j + 1, n):
+                clauses.append([-1 * v_pos[j],-1 * v_pos[k]])
+
+    for i in range(1, n + 1):
+        pos_v = [(j * n) + i for j in range(n)]
+        clauses.append(pos_v)
+        for j in range(0, n):
+            for k in range(j + 1, n):
+                clauses.append([-1 * pos_v[j],-1 * pos_v[k]])
+    edges_v = [[] for _ in range(n)]
+    for edge in edges:
+        edges_v[edge[0]-1].append(edge[1]-1)
+        edges_v[edge[1]-1].append(edge[0]-1)
+    for i in range(n):
+        for j in range(n):
+            if j not in edges_v[i] and (j != i):
+                for k in range(1,n):
+                    clauses.append(findVal(i,j,k))
+    print(len(clauses) , end = " ")
+    print(n*n)
+    for clause in clauses:
+        for v in clause:
+            print(v,end=" ")
+        print(0)
+def findVal(i,j,k):
+    return [-1 * ((i * n) + k), -1 * ((j * n) + k + 1)]
 
 printEquisatisfiableSatFormula()
--- Course5- Advanced Algorithms and Complexity/Week - 3/Programming-Assignment-3/gsm_network/gsm_network.py
+++ Course5- Advanced Algorithms and Complexity/Week - 3/Programming-Assignment-3/gsm_network/gsm_network.py
@@ -6,9 +6,29 @@ edges = [ list(map(int, input().split())) for i in range(m) ]
 # and passes about half of the tests.
 # Change this function to solve the problem.
 def printEquisatisfiableSatFormula():
-    print("3 2")
-    print("1 2 0")
-    print("-1 -2 0")
-    print("1 -2 0")
+    # print("3 2")
+    # print("1 2 0")
+    # print("-1 -2 0")
+    # print("1 -2 0")
+    clauses=[]
+    for i in range(n):
+        clauses.append([(i * 3 + 1), (i * 3 + 2), (i * 3 + 3)])
+        clauses.append([-1 * (i * 3 + 1), -1 * (i * 3 + 2)])
+        clauses.append([-1 * (i * 3 + 1), -1 * (i * 3 + 3)])
+        clauses.append([-1 * (i * 3 + 2), -1 * (i * 3 + 3)])
+    for edge in edges:
+        clauses.append([-1 * ((edge[0]-1) * 3 + 1), -1 * ((edge[1] -1)* 3 + 1)])
+        clauses.append([-1 * ((edge[0]-1) * 3 + 2), -1 * ((edge[1] -1)* 3 + 2)])
+        clauses.append([-1 * ((edge[0]-1) * 3 + 3), -1 * ((edge[1] -1)* 3 + 3)])
+    print(len(clauses),end = " ")
+    print(n*3)
+    for clause in clauses:
+        for i in clause:
+            print(i,end=" ")
+        print(0)
 
 printEquisatisfiableSatFormula()
+
+# print(n)
+# print(m)
+# print(edges)
